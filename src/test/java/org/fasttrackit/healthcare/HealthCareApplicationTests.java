4b9c5c3452ed1a96973f955edecd03cefac0fe79
laurentiu-meghesan <laurentiu.meghesan@gmail.com>
2020-03-02 21:35:28 +0200

    Initial commit (project structure)

---
+++ src/test/java/org/fasttrackit/healthcare/HealthCareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.healthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
